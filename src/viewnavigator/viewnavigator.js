5911f163e93a49e402bd2495587570f69cbbeb2e
Andrew Trice <triceam@gmail.com>
2012-04-11 11:58:33 -0400

    various minor changes

--- src/viewnavigator/viewnavigator.js
+++ src/viewnavigator/viewnavigator.js
@@ -69,18 +69,21 @@ ViewNavigator.prototype.pushView = function( viewDescriptor ) {
 
 ViewNavigator.prototype.popView = function() {
 
-	if (this.animating || this.history.length <= 1 )
-		return;
-	
-	var currentViewDescriptor = this.history[ this.history.length-1];
-	if ( currentViewDescriptor.backCallback ) {
-		currentViewDescriptor.backCallback();
-	}
+	if ( this.history.length > 1 ) {
+		if (this.animating || this.history.length <= 1 )
+			return;
 		
-	this.history.pop();	
-	var viewDescriptor = this.history[ this.history.length-1 ];
-	viewDescriptor.animation = "popEffect"
-	this.updateView( viewDescriptor );
+		var currentViewDescriptor = this.history[ this.history.length-1];
+		this.history.pop();	
+		
+		if ( currentViewDescriptor.backCallback ) {
+			currentViewDescriptor.backCallback();
+		}
+			
+		var viewDescriptor = this.history[ this.history.length-1 ];
+		viewDescriptor.animation = "popEffect"
+		this.updateView( viewDescriptor );
+	}
 }
 
 ViewNavigator.prototype.setHeaderPadding = function( amount ) {
